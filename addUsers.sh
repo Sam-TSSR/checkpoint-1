6c52623da72898c48b4b6960a258e94400cb8600
Sam-TSSR <samkilic77@gmail.com>
2024-10-25 12:07:26 +0200

    Add files via upload

---
+++ addUsers.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+read -p "Ecrivez les noms d'utilisateur que vous souhaitez ajouter " nom
+
+#verification
+
+if [ "$nom" == "" ]; then
+	echo "Il manque les noms d'utilisateurs en argument - Fin du script"
+    exit 1
+fi
+
+#on va utiliser for in
+
+	for i in $nom
+	do
+		if id "$i" &>/dev/null
+		then
+		echo "L'utilisateur $i existe déjà"
+		else
+		sudo useradd "$i"
+
+#verification si le code marche correctement		
+
+            if [ $? -eq 0 ]
+            then
+		    echo "L'utilisateur $i a été crée"
+            else
+            echo "Erreur à la création de l'utilisateur $i"
+            fi
+		fi
+	done
+	exit 0
+	
